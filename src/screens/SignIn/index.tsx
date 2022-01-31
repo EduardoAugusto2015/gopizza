b6aa5e570da7b81ad601f165d944a579d8940dbd
Eduardo Augusto Petreca <eduardopetreca@gmail.com>
2022-01-31 10:12:55 -0300

    create input and butto

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+import { Container } from './styles';
 
 export function SignIn(){
     return(
         <Container>
+            <Input
+                placeholder= "E-mail"
+                type="secondary"
+                autoCorrect={false}
+                autoCapitalize="none"
+            />
+            <Input
+                placeholder= "Senha"
+                type="secondary"
+                securityTextEntry={true}
+            />
+            <Button 
+                title="Entrar"
+                type="secondary"
+            />
         </Container>
     );
 }
