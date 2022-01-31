2699578f9d9f0eeb0091e3ea97dd0eed8fa83c23
Eduardo Augusto Petreca <eduardopetreca@gmail.com>
2022-01-31 12:57:13 -0300

    add hooks auth

--- babel.config.js
+++ babel.config.js
@@ -19,6 +19,7 @@ module.exports = function(api) {
                     '@components' : './src/components',
                     '@screens' : './src/screens',
                     '@assets' : './src/assets',
+                    '@hooks' : './src/hooks',
                   }
                 }
               ]
