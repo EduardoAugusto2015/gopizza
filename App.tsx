7e1be9b00a6f38aa432b85bbc958ca391c2edcbc
Eduardo Augusto Petreca <eduardopetreca@gmail.com>
2022-01-31 08:39:53 -0300

    add gradient, add statusbar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
+
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import { ThemeProvider } from 'styled-components/native';
 
 import theme from './src/theme';
+
 import { SignIn } from '@screens/SignIn';
 
 export default function App() {
@@ -20,6 +22,7 @@ export default function App() {
   
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style='light' translucent backgroundColor='transparent'/>
       <SignIn/>
     </ThemeProvider>
   );
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 export function SignIn(){
     return(
         <Container>
-            
         </Container>
     );
 }
