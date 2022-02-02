d06eab7550d4857f8f46bb55a4705bd6bf5f05cb
Eduardo Augusto Petreca <eduardopetreca@gmail.com>
2022-02-02 03:58:16 -0300

    implemented get user data firestore

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -7,10 +7,18 @@ import React, {
 
 import { Alert } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
+type User = {
+    id: string;
+    name: string;
+    isAdmin: boolean;
+}
 
 type AuthContextData = {
     signIn: (email: string, password: string) => Promise<void>;
     isLogging: boolean;
+    user: User | null;
 }
 
 type AuthProviderProps = {
@@ -21,6 +29,7 @@ export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps){
     const [isLogging, setIsLogging] = useState(false);
+    const [user, setUser] = useState<User | null>(null);
 
     async function signIn (email: string, password: string){
 
@@ -31,7 +40,22 @@ function AuthProvider({ children }: AuthProviderProps){
         auth()
         .signInWithEmailAndPassword(email, password)
         .then(account =>{
-            console.log(account)
+            firestore()
+            .collection('users')
+            .doc(account.user.uid)
+            .get()
+            .then(profile => {
+                const { name, isAdmin } = profile.data() as User;
+
+                if(profile.exists){
+                    const userData = {
+                        id: account.user.uid,
+                        name,
+                        isAdmin
+                    };
+                    setUser(userData);
+                }
+            }).catch(()=> Alert.alert('Login', 'Não foi possível buscar os dados de perfil do usuário'));
         })
         .catch( error => {
             const { code } = error;
@@ -47,7 +71,8 @@ function AuthProvider({ children }: AuthProviderProps){
     return (
         <AuthContext.Provider value={{
             signIn,
-            isLogging
+            isLogging,
+            user
         }}>
             {children}
         </AuthContext.Provider>
