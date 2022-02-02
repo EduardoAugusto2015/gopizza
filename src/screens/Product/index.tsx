a95dd4b44acb5484a8e352c572acd1f83cd52fc1
Eduardo Augusto Petreca <eduardopetreca@gmail.com>
2022-02-02 05:01:12 -0300

    start create screen product

---
+++ src/screens/Product/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Platform } from 'react-native';
+
+import { 
+    Container, 
+} from './styles';
+
+export function Product(){
+    return(
+        <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+
+        </Container>
+    );
+}
