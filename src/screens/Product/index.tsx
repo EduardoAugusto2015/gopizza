a700788bd6780b3d67f913ff81ec5970a0b59ec4
Eduardo Augusto Petreca <eduardopetreca@gmail.com>
2022-02-02 05:24:58 -0300

    start create header on screen product

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -3,12 +3,21 @@ import { Platform } from 'react-native';
 
 import { 
     Container, 
+    Header,
+    Title,
+    TouchableOpacity,
+    DeleteLabel
 } from './styles';
 
 export function Product(){
     return(
         <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-
+            <Header>
+                <Title>Cadastrar</Title>
+                <TouchableOpacity>
+                    <DeleteLabel>Deletar</DeleteLabel>
+                </TouchableOpacity>
+            </Header>
         </Container>
     );
 }
