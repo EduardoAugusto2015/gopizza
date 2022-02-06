b64ce4d18a42888a90b86532d49808478d2aff18
Eduardo Augusto Petreca <eduardopetreca@gmail.com>
2022-02-06 07:14:25 -0300

    implemented getData from firestore

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useEffect} from "react";
 import HappyEmoji from '@assets/happy.png';
+
+import firestore from "@react-native-firebase/firestore";
+
 import { MaterialIcons } from "@expo/vector-icons";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
 import { useTheme } from 'styled-components/native';
 import { 
     Container, 
@@ -14,10 +17,36 @@ import {
     Title,
  } from "./styles";
 import { Search } from '@components/Search';
-import { ProductCard } from '@components/ProductCard';
+import { ProductCard, ProductProps } from '@components/ProductCard';
 
 export function Home (){
-    const {COLORS} = useTheme();
+    const { COLORS } = useTheme();
+    
+    function fetchPizzas(value: string){
+        const formattedValue = value.toLocaleLowerCase().trim();
+
+        firestore()
+        .collection('pizzas')
+        .orderBy('name_insensitive')
+        .startAt(formattedValue)
+        .endAt(`${formattedValue}\uf8ff`)
+        .get()
+        .then(response => {
+            const data = response.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    ...doc.data(),
+                }
+            }) as ProductProps[];   
+            console.log(data);
+        })
+        .catch(()=> Alert.alert('Consulta', 'Não foi possivel realizar a consulta'))
+    }
+
+    useEffect(()=>{
+        fetchPizzas('');
+    }, [])
+
     
     return (
         <Container>
