0c5921929cdc9da650c3ca1f30ecda827f8baf46
Eduardo Augusto Petreca <eduardopetreca@gmail.com>
2022-02-06 16:41:08 -0300

    create image component in  Order screen

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Platform } from "react-native";
 import { ButtonBack } from "@components/ButtonBack";
-import { Container, Header } from './styles'
+import { 
+    Container, 
+    Header,
+    Photo 
+} from './styles'
 
 export function Order (){
     return (
@@ -12,6 +16,7 @@ export function Order (){
                     style={{ marginBottom: 108 }}
                 />
             </Header>
+            <Photo source={{uri: 'https://firebasestorage.googleapis.com/v0/b/gopizzaapp-8525a.appspot.com/o/pizzas%2F1644174662915.png?alt=media&token=b4c6e475-1ecf-484c-9769-172dc62e839c'}}/>
         </Container>
     )
 }
